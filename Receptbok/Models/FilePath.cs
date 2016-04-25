599191fe84fa0561908700001ede198866797937
Rasmus Lillvik <ZubZeon@null.net>
2016-04-25 10:29:04 +0300

    still testing

---
+++ Receptbok/Models/FilePath.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Receptbok.Models
+{
+    public class FilePath
+    {
+        public int FilePathId { get; set; }
+        [StringLength(255)]
+        public string FileName { get; set; }
+        public FileType FileType { get; set; }
+
+        public int RecipeId { get; set; }
+        public virtual Recipes Recipes { get; set; }
+    }
+}
---
+++ Receptbok/Models/Recipes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Receptbok.Models
+{
+    public partial class Recipes
+    {
+        [Key]
+        public int RecipeId { get; set; }
+        public string RecipeName { get; set; }
+        public string RecipeInstructions { get; set; }
+        public Nullable<int> CategoryId { get; set; }
+
+        public virtual Categories Categories { get; set; }
+        public virtual ICollection<FilePath> FilePath { get; set; }
+    }
+}
